424d83330057c2b8aa286bd4358a98156e11b979
Alessio Burrello <alessio.burrello@unibo.it>
2021-02-04 12:49:53 +0100

    Update Hyperbus drivers and FPGA patch

--- rtos/pulpos/pulp/kernel/chips/pulp/soc.c
+++ rtos/pulpos/pulp/kernel/chips/pulp/soc.c
@@ -23,7 +23,7 @@
 
 void pos_soc_init()
 {
-	#if __PLATFORM__ != ARCHI_PLATFORM
+	#if __PLATFORM__ != ARCHI_PLATFORM_FPGA
     pos_fll_constructor();
 
     pos_freq_domains[PI_FREQ_DOMAIN_FC] = pos_fll_init(POS_FLL_FC);
